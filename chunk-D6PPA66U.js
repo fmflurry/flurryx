85d9dc8786200cf8c2961b07671a6c98b892c533
fmflurry <fm.florian.michel@gmail.com>
2026-04-03 23:49:40 +0200

    Updates

---
+++ chunk-D6PPA66U.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-TS4P7EOA.js";import"./chunk-2VIICIXQ.js";export{a as TASKS_ROUTES};
---
+++ chunk-W37XC653.js
@@ -0,0 +1,1 @@
+import{d as a}from"./chunk-TS4P7EOA.js";import"./chunk-2VIICIXQ.js";export{a as PROJECTS_ROUTES};
